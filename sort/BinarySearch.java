f1cadec7dfa80362d6329d4b580c587f3e5669f2
bloo <nexushellstein@gmail.com>
2024-09-01 03:37:01 +0800

    Coded linear search in java

--- sort/BinarySearch.java
+++ sort/BinarySearch.java
@@ -1,3 +1,4 @@
+package sort;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] data = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
---
+++ sort/LinearSearch.java
@@ -0,0 +1,14 @@
+package sort;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] data = {43, 21, 0, 32, 4, 32, 54, 2, 10, 11};
+        int num = 32;
+
+        for(int i = 0; i < data.length; i++){
+            if(num == data[i]){
+                System.out.println("Number found at index " + i);
+            }
+        }
+    }
+}
