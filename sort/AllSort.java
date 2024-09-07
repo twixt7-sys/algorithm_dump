ba160a375b5c8882d5cf6921dd014f660d4eafcd
bloo <nexushellstein@gmail.com>
2024-09-07 20:05:02 +0800

    minor changes
    imported arrays in allsort
    coded merge sort algorithm in allsort

--- sort/AllSort.java
+++ sort/AllSort.java
@@ -1,5 +1,6 @@
 package sort;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -101,6 +102,38 @@ public class AllSort {
     }
 
     public static int[] merge_sort(int[] data){
+        if(data.length > 1){
+            int mid = data.length / 2;
+            int[] left = Arrays.copyOfRange(data, 0, mid);
+            int[] right = Arrays.copyOfRange(data, mid, data.length);
+
+            merge_sort(left);
+            merge_sort(right);
+
+            int i = 0, j = 0, k = 0;
+            while (i < left.length && j < right.length){
+                if (left[i] <= right[j]){
+                    data[k] = left[i];
+                    i++;
+                }else{
+                    data[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while(i < left.length){
+                data[k] = left[i];
+                i++;
+                k++;
+            }
+
+            while(j < right.length){
+                data[k] = right[j];
+                j++;
+                k++;
+            }
+        }
         return data;
     }
 
