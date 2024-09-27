dd41f81259c222880823191ce4bdf9c1a894e688
bloo <nexushellstein@gmail.com>
2024-09-27 23:26:33 +0800

    coded problem 3

---
+++ problemset1/prob3.java
@@ -0,0 +1,14 @@
+package problemset1;
+
+// Print the multiplication table
+
+public class prob3 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 15; i++){
+            for(int j = 1; j <= 20; j++){
+                System.out.print((i * j) + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
