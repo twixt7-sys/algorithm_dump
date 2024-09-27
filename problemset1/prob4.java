db36e82a172d37b7274d38cfaf1396eaab7053b8
bloo <nexushellstein@gmail.com>
2024-09-28 00:00:32 +0800

    coded problem 4

---
+++ problemset1/prob4.java
@@ -0,0 +1,19 @@
+package problemset1;
+
+// Print the multiplication table with color
+
+public class prob4 {
+    public static void main(String[] args) {
+        String RESET = "\u001B[0m";
+        String GREEN = "\u001B[32m";
+        String YELLOW = "\u001B[33m";
+
+        for(int i = 1; i < 20; i++){
+            for(int j = 1; j < 20; j++){
+                if(i == 1 || j == 1) System.out.print(GREEN + (i * j) + "\t" + RESET);
+                else System.out.print(YELLOW + (i * j) + "\t" + RESET);
+            }
+            System.out.println();
+        }
+    }
+}
