672d5ea0bbd5d33d054c56028393c685ec95fdbc
bloo <nexushellstein@gmail.com>
2024-09-27 23:21:53 +0800

    made probset 1
    - coded problem 1
    - coded problem 2

---
+++ problemset1/prob1.java
@@ -0,0 +1,10 @@
+package problemset1;
+
+
+// Print numbers from 1 to 10
+
+public class prob1{
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++) System.out.print(i + "   ");
+    }
+}
---
+++ problemset1/prob2.java
@@ -0,0 +1,9 @@
+package problemset1;
+
+// Print odd numbers less than 100
+
+public class prob2 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 100; i++) if(i % 2 == 1) System.out.println(i);
+    }
+}
