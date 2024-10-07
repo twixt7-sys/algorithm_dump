50e11a78786efb6be423abcd6b528adfd9f1841e
bloo <nexushellstein@gmail.com>
2024-10-07 23:09:45 +0800

    minor changes

--- problemset1/prob24.java
+++ problemset1/prob24.java
@@ -1,3 +1,5 @@
+// Create a function that merges two arrays as a new array
+
 public class prob24 {
     public static void main(String[] args) {
         int[] data1 = {41,52,36,748,5473,625};
@@ -9,8 +11,8 @@ public class prob24 {
     }
     static int[] merge(int[] data1, int[] data2){
         int[] data = new int[data1.length + data2.length];
-        for(int i = 0; i < data1.length - 1; i++) data[i] = data1[i];
-        for(int i = data1.length + 1; i < data1.length + data2.length - 2; i++) data[i] = data2[i - data1.length - 1];
+        for(int i = 0; i < data1.length; i++) data[i] = data1[i];
+        for(int i = 0; i < data2.length; i++) data[i + data1.length] = data2[i];
         return data;
     }
 }
---
+++ problemset1/prob25.java
@@ -0,0 +1,27 @@
+//  Create a function that takes two arrays as parameters and returns a new array with the elements that are only in one array(not in both)
+import java.util.LinkedList;
+
+public class prob25 {
+    public static void main(String[] args) {
+        int[] data1 = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] data2 = {3, 4, 5, 6, 7, 8, 9, 10};
+
+        int[] data = get_solo_elements(data1, data2);
+    }
+    static int[] get_solo_elements(int[] data1, int[] data2){
+        LinkedList<Integer> list = new LinkedList<>();
+        
+        for(int i = 0; i < data1.length; i++){
+
+        }
+
+        for(int )
+        list.contains()
+
+        int count = 0;
+        for(int i = 
+    )
+        
+        return data;
+    }
+}
